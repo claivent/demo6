fdcfab1f238df44780a109ea767f9cdeb5c61d3a
Martin Saska <claivent@gmail.com>
2026-05-28 11:48:06 +0200

    init11

--- src/main/frontend/src/app/users/users/users.component.ts
+++ src/main/frontend/src/app/users/users/users.component.ts
@@ -13,23 +13,25 @@ import {CommonModule} from '@angular/common';
 export class UsersComponent implements OnInit {
   users: User[] = [];
   error: string | null = null;
+  loading = true;
 
   constructor(private userService: UserService) {}
-
 
   ngOnInit(): void{
     this.userService.getUsers().subscribe({
       next: (data) => {
         this.users = data;
-
+        this.loading = false;
         if (data.length === 0) {
-          this.error = "no users"
+          this.error = "no users";
         } else {
-          this.error = null ;
+          this.error = null;
         }
       },
-
-      error: () => this.error = "nestáhli se uživatelé"
+      error: () => {
+        this.error = "nestáhli se uživatelé";
+        this.loading = false;
+      }
     });
   }
 }
