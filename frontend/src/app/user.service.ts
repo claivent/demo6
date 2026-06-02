61786381153701a80e9e5a06e6657a261035f21f
Martin SASKA <claivent@gmail.com>
2026-06-03 00:14:20 +0200

    edit: correct api.url

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -5,7 +5,7 @@ import { User } from './user.model';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/api/users';
+  private apiUrl = '/api/users';
   private apiUrl1 ='https://fakeapi.net/products'  ;
 
   constructor(private http: HttpClient) {}
